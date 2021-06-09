e7b08a2461cd5f792f553272a9e24da67e53de89
Andy Lulham <a.lulham@gmail.com>
2021-06-09 14:25:02 +0100

    Remove guidance and developer documentation

--- gen_rst.sh
+++ gen_rst.sh
@@ -18,18 +18,8 @@ cp IATI-Rulesets/SPEC.rst docs/en/rulesets/ruleset-spec.rst
 
 cd docs || exit 1
 
-mkdir en/developer
-cp -n ../IATI-Developer-Documentation/*.rst en/developer
-cp -rn ../IATI-Developer-Documentation/*/ en/developer
-mkdir en/guidance
-cp -n ../IATI-Guidance/en/*.rst en/guidance
-cp -rn ../IATI-Guidance/en/*/ en/guidance
-mv en/guidance/404.rst en/
-mv en/guidance/upgrades* en/
-mv en/guidance/introduction* en/
-mv en/guidance/key-considerations* en/
-mv en/guidance/license* en/
 cp ../combined_sitemap.rst en/sitemap.rst
+mv en/reference.rst en/index.rst
 
 cp ../conf.py en/
 
--- reset.sh
+++ reset.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-rm -rf IATI-Developer-Documentation
-rm -rf IATI-Guidance
 rm -rf IATI-Codelists
 rm -rf IATI-Extra-Documentation
 rm -rf IATI-Rulesets
 rm -rf IATI-Schemas
 rm -rf docs
 unlink iatirulesets
-
-git clone https://github.com/IATI/IATI-Developer-Documentation.git
-
-git clone https://github.com/IATI/IATI-Guidance.git
-cd IATI-Guidance
-# reset to an old version
-git reset --hard c79fd1e3
-cd ..
 
 git clone --branch version-2.03 https://github.com/IATI/IATI-Codelists.git
 
